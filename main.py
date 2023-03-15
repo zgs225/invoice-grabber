b12c0be7bebe7550010dc3be74990b231d454759
zgs225 <zgs225@gmail.com>
2023-03-15 08:09:29 +0800

    feat: 添加下载全家发票

--- main.py
+++ main.py
@@ -3,12 +3,21 @@ from imaplib import IMAP4_SSL
 import datetime
 import email
 import os
+from selenium.webdriver.common.by import By
 import socks
+import re
+import requests
+
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+import time
+
 
 def load_config():
     # Load config from json file
     with open("config.json") as f:
         return json.load(f)
+
 
 def check_and_rename_file(dirpath, filepath):
     filename = os.path.basename(filepath)
@@ -18,6 +27,95 @@ def check_and_rename_file(dirpath, filepath):
         filename = f"{base}_{i}{ext}"
         i += 1
     return os.path.join(dirpath, filename)
+
+
+def try_download_file_from_quanjia(cfg, msg):
+    # Get the email subject
+    subject = msg["Subject"]
+    if subject is None:
+        print("No subject for message")
+        return
+
+    if subject.startswith("=?"):
+        subject = email.header.decode_header(subject)[0][0]
+        try:
+            subject = subject.decode("utf-8")
+        except:
+            print("Cannot decode subject")
+            return False
+
+    if "顶全便利店" not in subject:
+        return False
+
+    print(f"Subject: {subject} (from 全家便利店)")
+
+    body = msg.get_payload(decode=True)
+    if body is None:
+        print("No body for message")
+        return True
+    body = body.decode("utf-8")
+    if 'href="' not in body:
+        print("No href in body")
+        return True
+
+    # Get the download link
+    result = re.search(r"href=['\"](https?://fpj.datarj.com/[^\s\"']*)['\"]", body)
+    if result is None:
+        print("No download page link found")
+        return True
+
+    download_link = result.group(1)
+    print(f"Download page link: {download_link}")
+
+    # request download link, match r"href=['\"](https?://fpj.datarj.com/e-invoice-file/[^'\"]+.pdf)['\"]" from response body
+    opts = Options()
+    opts.add_argument("--headless")
+    browser = Firefox(options=opts)
+    browser.implicitly_wait(30)
+
+    try:
+        browser.get(download_link)
+        time.sleep(1)
+    except Exception as e:
+        print(f"Error opening download page: {e}")
+        return True
+
+    links = browser.find_elements(By.TAG_NAME, "a")
+
+    for link in links:
+        href = link.get_dom_attribute("href")
+
+        if href is None:
+            continue
+
+        href = href.strip()
+        result = re.match(r"https?://fpj.datarj.com/e-invoice-file/[^'\"]+.pdf", href)
+        if result is None:
+            continue
+
+        print(f"Found download link: {href}")
+        print("Downloading...")
+
+        filename = os.path.basename(href)
+        download_dir = cfg["download_dir"]
+        response = requests.get(href)
+
+        if response.status_code != 200:
+            print("Error downloading file")
+            continue
+
+        filepath = check_and_rename_file(download_dir, filename)
+
+        os.makedirs(download_dir, exist_ok=True)
+        with open(filepath, "wb+") as f:
+            f.write(response.content)
+
+        print(f"Downloaded {filename} to {filepath}")
+
+    browser.close()
+
+    return True
+
 
 def roam(cfg):
     # Set proxy if needed
@@ -59,6 +157,10 @@ def roam(cfg):
             continue
         raw_email = data[0][1]
         msg = email.message_from_bytes(raw_email)
+
+        if try_download_file_from_quanjia(cfg, msg):
+            continue
+
         for part in msg.walk():
             if part.get_content_type() == "multipart":
                 continue
@@ -77,7 +179,6 @@ def roam(cfg):
                 print(f"Skipping {filename} (not a PDF)")
                 continue
 
-            # create download dir if needed
             download_dir = cfg["download_dir"]
             os.makedirs(download_dir, exist_ok=True)
             filepath = check_and_rename_file(download_dir, filename)
